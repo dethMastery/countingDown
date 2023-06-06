af7388c9331c69ad71997c41808d71c8a0f97a60
dethMastery <me@suphakit.net>
2023-06-06 23:37:05 +0700

    chore: theme button updated

--- src/js/mode.js
+++ src/js/mode.js
@@ -1,8 +1,12 @@
-let theme = localStorage.getItem('theme')
+let theme = (localStorage.getItem('theme') == 'dark')
 let element = document.body
+let button = document.querySelector('#themeButton')
 
-if (theme == 'dark') {
+if (theme) {
   element.classList.add('dark')
+  button.innerHTML = '☀️'
+} else {
+  button.innerHTML = '🌙'
 }
 
 function mode() {
@@ -10,7 +14,9 @@ function mode() {
   
   if (element.classList.value == 'dark') {
     localStorage.setItem('theme', "dark")
+    button.innerHTML = '☀️'
   } else {
     localStorage.setItem('theme', "light")
+    button.innerHTML = '🌙'
   }
 }
