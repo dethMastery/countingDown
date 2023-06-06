db2ca76407b4afac69af54a5443a04f8316fc1fa
dethMastery <me@suphakit.net>
2023-06-07 00:19:28 +0700

    feat: monthNo -> monthName

---
+++ src/js/month.js
@@ -0,0 +1,20 @@
+export default function month(int) {
+  const monthList = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+
+  let month = monthList[parseInt(int) - 1]
+
+  return month
+}
