5c9994082a0f56f5e5bbbb357abfa4805ede8c47
Suphakit P <me@suphakit.net>
2023-06-15 15:26:54 +0700

    chore: time showing errors

--- src/js/count.js
+++ src/js/count.js
@@ -5,9 +5,9 @@ let x = setInterval(function () {
 
   // let day = Math.floor(currentTime / (1000 * 60 * 60 * 24))
   let hour = Math.floor((currentTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  let min = Math.floor((currentTime % (1000 * 60 * 60)) / (1000 * 60))
-  let sec = Math.floor((currentTime % (1000 * 60)) / 1000)
-  let dateLine = `${hour} : ${min} : ${sec}`
+  let min = Math.floor((currentTime % (1000 * 60 * 60)) / (1000 * 60)).toString()
+  let sec = Math.floor((currentTime % (1000 * 60)) / 1000).toString()
+  let dateLine = `${hour % 12}:${(min.length < 2) ? `0${min}`: min}:${(sec.length < 2) ? `0${sec}` : sec}  ${(hour > 12) ? 'PM' : 'AM'}`
   countingContainer.innerHTML = `<h1>
   ${dateLine}
 </h1>`
